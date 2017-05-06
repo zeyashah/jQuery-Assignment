f1bb70cb706424c2682407a0eca6a8dfe04633f7
Zeya Shah <8zeya8@gmail.com>
2017-05-06 00:56:59 -0400

    added comments to code

--- jQueryBasics.js
+++ jQueryBasics.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 	
 	
-	$(".liner").hide();
+	$(".liner").hide(); // to hide everything on the page, until the box is clicked
 
 	$(".box").animate({
-		width: "130px",  // see note at bottom 
+		width: "130px",   
 		height: "70px",
 		opacity: 0.5
 	}, 2000, function() {
@@ -14,6 +14,7 @@ $(document).ready(function() {
 		}, 50)
 	});
 
+// click box to reveal page, slide the box up and make the headings and paragraphs visible
 	$(".box").click(function() {
 		$(".liner").show();
 		$(".box").slideUp(1000, function() {
@@ -22,6 +23,7 @@ $(document).ready(function() {
 		});
 	});
 
+// hide overlay text when mouse hovers over picture, overlay appears again when mouse leaves the picture
 	$(".image1").on("mouseenter", function() {
 	$("h4").fadeOut(1000);
 	});
@@ -30,10 +32,12 @@ $(document).ready(function() {
 	$("h4").fadeIn(500);
 	});
 
+// change color of last 2 paragraphs of text by hovering over the text
 	$(".paragraph").hover(function() {
 		$(".paragraph p").css("color","red");
 	});
 
+// slide the 2nd picture to the right then slideup to remove from screen, changing the above text back to original color
 	$(".image2").click(function() {
 		$(".image2").animate(
 		{'margin-left':'1000px'}, 5000,
@@ -41,8 +45,6 @@ $(document).ready(function() {
 			$(".image2").slideUp('fast')
 			$(".paragraph p").css("color","black")
 		});
-
 	});	
+
 });
-
-
